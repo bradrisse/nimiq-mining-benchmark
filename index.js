e6ada9083a7ca31e32bbc33db9c8dc99716c9ae1
Brad Risse <brad@omniholding.com>
2018-03-01 14:22:39 -0800

    Type log fix

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function runNimiq(_compData) {
     console.log('running nimiq miner...')
     const $ = {};
     const TAG = 'Node';
+    const type = 'light';
 
     Nimiq.Log.instance.level = 'LOG_DISABLE';
 
